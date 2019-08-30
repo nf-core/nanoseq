85076dac8ef65858be7c68c867b469b14ab1d42e
drpatelh <drhpatel@gmail.com>
2019-08-30 14:29:04 +0100

    Add in software versions and MultiQC

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -2,27 +2,43 @@
 from __future__ import print_function
 from collections import OrderedDict
 import re
+import os
 
 # TODO nf-core: Add additional regexes for new tools in process get_software_versions
 regexes = {
-    'nf-core/nanodemux': ['v_pipeline.txt', r"(\S+)"],
-    'Nextflow': ['v_nextflow.txt', r"(\S+)"],
-    'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
-    'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
+    'nf-core/nanodemux': ['pipeline.version', r"(\S+)"],
+    'Nextflow': ['nextflow.version', r"(\S+)"],
+    'guppy': ['guppy.version', r"Version (\S+)"],
+    'pycoQC': ['pycoqc.version', r"pycoQC v(\S+)"],
+    'NanoPlot': ['nanoplot.version', r"NanoPlot (\S+)"],
+    'GraphMap': ['graphmap.version', r"(\S+)"],
+    'minimap2': ['minimap2.version', r"(\S+)"],
+    'Samtools': ['samtools.version', r"samtools (\S+)"],
+    'rmarkdown': ['rmarkdown.version', r"(\S+)"],
+    'MultiQC': ['multiqc.version', r"multiqc, version (\S+)"],
 }
 results = OrderedDict()
 results['nf-core/nanodemux'] = '<span style="color:#999999;\">N/A</span>'
 results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
-results['FastQC'] = '<span style="color:#999999;\">N/A</span>'
+results['guppy'] = '<span style="color:#999999;\">N/A</span>'
+results['pycoQC'] = '<span style="color:#999999;\">N/A</span>'
+results['NanoPlot'] = '<span style="color:#999999;\">N/A</span>'
+results['GraphMap'] = '<span style="color:#999999;\">N/A</span>'
+results['minimap2'] = '<span style="color:#999999;\">N/A</span>'
+results['Samtools'] = '<span style="color:#999999;\">N/A</span>'
+results['rmarkdown'] = '<span style="color:#999999;\">N/A</span>'
 results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
 for k, v in regexes.items():
-    with open(v[0]) as x:
-        versions = x.read()
-        match = re.search(v[1], versions)
-        if match:
-            results[k] = "v{}".format(match.group(1))
+    if os.path.exists(v[0]):
+        with open(v[0]) as x:
+            versions = x.read()
+            match = re.search(v[1], versions)
+            if match:
+                results[k] = "v{}".format(match.group(1))
+    else:
+        results[k] = False
 
 # Remove software set to false in results
 for k in results:
