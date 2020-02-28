d73077d6553375053bea11325ce053c0656b7591
drpatelh <drhpatel@gmail.com>
2020-02-28 16:25:52 +0000

    Rewrite check_samplesheet.py script

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -1,124 +1,124 @@
 #!/usr/bin/env python
 
-#######################################################################
-#######################################################################
-## Created on August 28th 2019 to check nf-core/nanoseq design file
-#######################################################################
-#######################################################################
+###########################################################################
+###########################################################################
+## Created on August 28th 2019 to check nf-core/nanoseq samplesheet file
+###########################################################################
+###########################################################################
 
 import os
 import sys
 import argparse
 
-############################################
-############################################
-## PARSE ARGUMENTS
-############################################
-############################################
+def parse_args(args=None):
+    Description = 'Reformat nf-core/nanoseq samplesheet file and check its contents.'
+    Epilog = """Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"""
 
-Description = 'Reformat nf-core/nanoseq design file and check its contents.'
-Epilog = """Example usage: python check_samplesheet.py <DESIGN_FILE_IN> <DESIGN_FILE_OUT>"""
+    parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+    parser.add_argument('FILE_IN', help="Input samplesheet file.")
+    parser.add_argument('FILE_OUT', help="Output samplesheet file.")
 
-argParser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+    return parser.parse_args(args)
 
-## REQUIRED PARAMETERS
-argParser.add_argument('DESIGN_FILE_IN', help="Input design file.")
-argParser.add_argument('DESIGN_FILE_OUT', help="Output design file.")
-args = argParser.parse_args()
+def print_error(error,line):
+    print("ERROR: Please check samplesheet -> {}\nLine: '{}'".format(error,line.strip()))
 
-############################################
-############################################
-## MAIN SCRIPT
-############################################
-############################################
+def check_samplesheet(FileIn,FileOut):
+    HEADER = ['sample', 'fastq', 'barcode', 'genome', 'transcriptome']
 
-ERROR_STR = 'ERROR: Please check samplesheet'
-HEADER = ['sample', 'fastq', 'barcode', 'genome', 'transcriptome']
+    ## CHECK HEADER
+    fin = open(FileIn,'r')
+    header = fin.readline().strip().split(',')
+    if header != HEADER:
+        print("ERROR: Please check samplesheet header -> {} != {}".format(','.join(header),','.join(HEADER)))
+        sys.exit(1)
 
-## CHECK HEADER
-fin = open(args.DESIGN_FILE_IN,'r')
-header = fin.readline().strip().split(',')
-if header != HEADER:
-    print("{} header: {} != {}".format(ERROR_STR,','.join(header),','.join(HEADER)))
-    sys.exit(1)
+    outLines = []
+    while True:
+        line = fin.readline()
+        if line:
+            lspl = [x.strip() for x in line.strip().split(',')]
 
-outLines = []
-while True:
-    line = fin.readline()
-    if line:
-        lspl = [x.strip() for x in line.strip().split(',')]
-        sample,fastq,barcode,genome,transcriptome = lspl
-
-        ## CHECK VALID NUMBER OF COLUMNS PER SAMPLE
-        numCols = len([x for x in lspl if x])
-        if numCols < 2:
-            print("{}: Invalid number of columns (minimum of 2)!\nLine: '{}'".format(ERROR_STR,line.strip()))
-            sys.exit(1)
-
-        ## CHECK SAMPLE ID ENTRIES
-        if sample:
-            if sample.find(' ') != -1:
-                print("{}: Sample ID contains spaces!\nLine: '{}'".format(ERROR_STR,line.strip()))
+            ## CHECK VALID NUMBER OF COLUMNS PER SAMPLE
+            numCols = len([x for x in lspl[:3] if x])
+            if numCols < 2:
+                print_error("Please specify 'sample' entry along with either 'fastq' or 'barcode'!",line)
                 sys.exit(1)
+
+            ## CHECK SAMPLE ID ENTRIES
+            sample,fastq,barcode,genome,transcriptome = lspl
+            if sample:
+                if sample.find(' ') != -1:
+                    print_error("Sample entry contains spaces!",line)
+                    sys.exit(1)
+            else:
+                print_error("Sample entry has not been specified!",line)
+                sys.exit(1)
+
+            ## CHECK BARCODE ENTRIES
+            if barcode:
+                if not barcode.isdigit():
+                    print_error("Barcode entry is not an integer!",line)
+                    sys.exit(1)
+                else:
+                    barcode = 'barcode%s' % (barcode.zfill(2))
+
+            ## CHECK FASTQ ENTRIES
+            if fastq:
+                if fastq[-9:] != '.fastq.gz' and fastq[-6:] != '.fq.gz':
+                    print_error("FastQ file does not have extension '.fastq.gz' or '.fq.gz'!",line)
+                    sys.exit(1)
+
+            ## CHECK GENOME ENTRIES
+            if genome:
+                if genome.find(' ') != -1:
+                    print_error("Genome entry contains spaces!",line)
+                    sys.exit(1)
+
+                if len(genome.split('.')) > 1:
+                    if genome[-6:] != '.fasta' and genome[-3:] != '.fa' and genome[-9:] != '.fasta.gz' and genome[-6:] != '.fa.gz':
+                        print_error("Genome entry does not have extension '.fasta', '.fa', '.fasta.gz' or '.fa.gz'!",line)
+                        sys.exit(1)
+
+            ## CHECK TRANSCRIPTOME ENTRIES
+            gtf = ''
+            is_transcripts = '0'
+            if transcriptome:
+
+                if transcriptome.find(' ') != -1:
+                    print_error("Transcriptome entry contains spaces!",line)
+                    sys.exit(1)
+
+                if transcriptome[-6:] != '.fasta' and transcriptome[-3:] != '.fa' and transcriptome[-9:] != '.fasta.gz' and transcriptome[-6:] != '.fa.gz' and transcriptome[-4:] != '.gtf' and transcriptome[-7:] != '.gtf.gz':
+                    print_error("Transcriptome entry does not have extension '.fasta', '.fa', '.fasta.gz', '.fa.gz', '.gtf' or '.gtf.gz'!",line)
+                    sys.exit(1)
+
+                if transcriptome[-4:] == '.gtf' or transcriptome[-7:] == '.gtf.gz':
+                    gtf = transcriptome
+                    if not genome:
+                        print_error("If genome isn't provided, transcriptome must be in fasta format for mapping!",line)
+                        sys.exit(1)
+                else:
+                    is_transcripts = '1'
+                    genome = transcriptome
+
+            outLines.append([sample,fastq,barcode,genome,gtf,is_transcripts])
         else:
-            print("{}: Sample ID not specified!\nLine: '{}'".format(ERROR_STR,line.strip()))
-            sys.exit(1)
+            fin.close()
+            break
 
-        ## CHECK BARCODE ENTRIES
-        if barcode:
-            if not barcode.isdigit():
-                print("{}: Barcode not an integer!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                sys.exit(1)
-            else:
-                barcode = 'barcode%s' % (barcode.zfill(2))
+    ## WRITE TO FILE
+    fout = open(FileOut,'w')
+    fout.write(','.join(['sample', 'fastq', 'barcode', 'genome', 'gtf', 'is_transcripts']) + '\n')
+    for line in outLines:
+        fout.write(','.join(line) + '\n')
+    fout.close()
 
-        ## CHECK FASTQ ENTRIES
-        if fastq:
-            if fastq[-9:] != '.fastq.gz' and fastq[-6:] != '.fq.gz':
-                print("{}: FastQ file has incorrect extension (has to be '.fastq.gz' or '.fq.gz')!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                sys.exit(1)
 
-        ## CHECK GENOME ENTRIES
-        if genome:
-            if genome.find(' ') != -1:
-                print("{}: Genome field contains spaces!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                sys.exit(1)
+def main(args=None):
+    args = parse_args(args)
+    check_samplesheet(args.FILE_IN,args.FILE_OUT)
 
-            if len(genome.split('.')) > 1:
-                if genome[-6:] != '.fasta' and genome[-3:] != '.fa' and genome[-9:] != '.fasta.gz' and genome[-6:] != '.fa.gz':
-                    print("{}: Genome field incorrect extension (has to be '.fasta', '.fa', '.fasta.gz' or '.fa.gz')!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                    sys.exit(1)
 
-        ## CHECK TRANSCRIPTOME ENTRIES
-        gtf = ''
-        is_transcripts = '0'
-        if transcriptome:
-
-            if transcriptome.find(' ') != -1:
-                print("{}: Transcriptome field contains spaces!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                sys.exit(1)
-
-            if transcriptome[-6:] != '.fasta' and transcriptome[-3:] != '.fa' and transcriptome[-9:] != '.fasta.gz' and transcriptome[-6:] != '.fa.gz' and transcriptome[-4:] != '.gtf' and transcriptome[-7:] != '.gtf.gz':
-                print("{}: Transcriptome field incorrect extension (has to be '.fasta', '.fa', '.fasta.gz', '.fa.gz', '.gtf' or '.gtf.gz')!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                sys.exit(1)
-
-            if transcriptome[-4:] == '.gtf' or transcriptome[-7:] == '.gtf.gz':
-                gtf = transcriptome
-                if not genome:
-                    print("{}: If genome isn't provided, transcriptome must be in fasta format for mapping!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                    sys.exit(1)
-            else:
-                is_transcripts = '1'
-                genome = transcriptome
-
-        outLines.append([sample,fastq,barcode,genome,gtf,is_transcripts])
-    else:
-        fin.close()
-        break
-
-## WRITE TO FILE
-fout = open(args.DESIGN_FILE_OUT,'w')
-fout.write(','.join(['sample', 'fastq', 'barcode', 'genome', 'gtf', 'is_transcripts']) + '\n')
-for line in outLines:
-    fout.write(','.join(line) + '\n')
-fout.close()
+if __name__ == '__main__':
+    sys.exit(main())
