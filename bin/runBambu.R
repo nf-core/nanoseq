f6b6910c469a8f11a307622888c0e2f3ce342114
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-07-15 11:40:29 +0800

    remove installation of BSgenome
    -the pipeline requires a fasta genome input to start with

--- bin/runBambu.R
+++ bin/runBambu.R
@@ -4,32 +4,14 @@
 ##load bambu
 library(bambu)
 
-if (!requireNamespace("BSgenome", quietly = TRUE)){
-  
-  install.packages("BiocManager", repos='http://cran.us.r-project.org')
-  BiocManager::install("BSgenome",update = FALSE, ask= FALSE)
-  
-  
-}
-
-
 args = commandArgs(trailingOnly=TRUE)
 
 readlist <- dir(args[1],full.names = TRUE, pattern = ".bam$")
 output_tag <- args[2]
 ncore <- args[3]
 
-if (length(args) < 4) {
+if (length(args) < 5) {
   stop("Please input the fullpath for the present directory and the sample sheet.", call.=FALSE)
-} else if (length(args)==4) {
-  if (!requireNamespace("BiocManager", quietly = TRUE)){
-    install.packages("BiocManager", repos='http://cran.us.r-project.org')
-  }
-  if (!require("BSgenome.Hsapiens.NCBI.GRCh38")){
-    BiocManager::install("BSgenome.Hsapiens.NCBI.GRCh38",update = FALSE, ask= FALSE)
-    library(BSgenome.Hsapiens.NCBI.GRCh38)
-  }
-  genomeseq <- "BSgenome.Hsapiens.NCBI.GRCh38"  #use BSgenome if fasta file is not provided
 } else {
   genomeseq <- args[5]
   genomeSequence <- Rsamtools::FaFile(genomeseq)
