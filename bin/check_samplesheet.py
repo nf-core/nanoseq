918a5d54041c7d4870da4fc2c94ed1589bfa661c
drpatelh <drhpatel@gmail.com>
2020-10-28 15:26:19 +0000

    Complete re-write

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -5,149 +5,155 @@ import sys
 import argparse
 
 def parse_args(args=None):
-    Description = 'Reformat nf-core/nanoseq samplesheet file and check its contents.'
-    Epilog = """Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"""
+    Description = "Reformat nf-core/nanoseq samplesheet file and check its contents."
+    Epilog = "Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
-    parser.add_argument('FILE_IN', help="Input samplesheet file.")
-    parser.add_argument('FILE_OUT', help="Output samplesheet file.")
-    parser.add_argument('INPUT_PATH', help="Input path for files.")
-
+    parser.add_argument("FILE_IN", help="Input samplesheet file.")
+    parser.add_argument("FILE_OUT", help="Output file.")
     return parser.parse_args(args)
 
 
-def print_error(error,line):
-    print("ERROR: Please check samplesheet -> {}\nLine: '{}'".format(error,line.strip()))
+def make_dir(path):
+    if len(path) > 0:
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise exception
+
+def print_error(error, context='Line', context_str=''):
+    error_str = "ERROR: Please check samplesheet -> {}".format(error)
+    if context != '' and context_str != '':
+        error_str = "ERROR: Please check samplesheet -> {}\n{}: '{}'".format(error, context.strip(), context_str.strip())
+    print(error_str)
+    sys.exit(1)
 
 
-def check_samplesheet(FileIn,FileOut,InputPath):
-    HEADER = ['group','replicate','barcode','input_file','genome','transcriptome']
+def check_samplesheet(file_in, file_out):
+    """
+    This function checks that the samplesheet follows the following structure:
 
-    ## CHECK HEADER
-    fin = open(FileIn,'r')
-    header = fin.readline().strip().split(',')
-    if header != HEADER:
-        print("ERROR: Please check samplesheet header -> {} != {}".format(','.join(header),','.join(HEADER)))
-        sys.exit(1)
+    group,replicate,barcode,input_file,genome,transcriptome
+    MCF7,1,,MCF7_directcDNA_replicate1.fastq.gz,genome.fa,genome.gtf
+    MCF7,2,,MCF7_directcDNA_replicate3.fastq.gz,genome.fa,genome.gtf
+    K562,1,,K562_directcDNA_replicate1.fastq.gz,genome.fa,genome.gtf
+    K562,2,,K562_directcDNA_replicate4.fastq.gz,genome.fa,genome.gtf
+    """
 
-    outLines, groups = [], {}
-    while True:
-        line = fin.readline()
-        if line:
-            lspl = [x.strip() for x in line.strip().split(',')]
+    sample_info_dict = {}
+    with open(file_in, "r") as fin:
 
-            ## CHECK VALID NUMBER OF COLUMNS PER SAMPLE (REQUIRE EITHER GROUP OR INPUT_FILE)
-            numCols = len([x for x in lspl[:4] if x])
-            if numCols < 2:
-                print_error("Please specify 'sample' entry along with either 'fastq' or 'barcode'!",line)
-                sys.exit(1)
+        ## Check header
+        MIN_COLS = 3
+        HEADER = ['group', 'replicate', 'barcode', 'input_file', 'genome', 'transcriptome']
+        header = fin.readline().strip().split(",")
+        if header[:len(HEADER)] != HEADER:
+            print("ERROR: Please check samplesheet header -> {} != {}".format(",".join(header), ",".join(HEADER)))
+            sys.exit(1)
 
-            ## ASSIGN ENTRIES TO VARIABLES
-            group,replicate,barcode,input_file,genome,transcriptome = lspl
+        ## Check sample entries
+        for line in fin:
+            lspl = [x.strip() for x in line.strip().split(",")]
 
-            ## CHECK GROUP ENTRIES
+            ## Check valid number of columns per row
+            if len(lspl) < len(HEADER):
+                print_error("Invalid number of columns (minimum = {})!".format(len(HEADER)), 'Line', line)
+
+            num_cols = len([x for x in lspl if x])
+            if num_cols < MIN_COLS:
+                print_error("Invalid number of populated columns (minimum = {})!".format(MIN_COLS), 'Line', line)
+
+            ## Check group name entries
+            group, replicate, barcode, input_file, genome, transcriptome = lspl[:len(HEADER)]
             if group:
-                if group.find(' ') != -1:
-                    print("{}: Group id contains spaces!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                    sys.exit(1)
-                else:
-                    if group not in groups:
-                        groups[group] = 1
-                    else:
-                        groups[group] += 1
-
-            ## CHECK REPLICATE ENTRIES
-            if replicate:
-                if not replicate.isdigit():
-                    print("{}: Replicate id not an integer!\nLine: '{}'".format(ERROR_STR,line.strip()))
-                    sys.exit(1)
-
-            ## CHECK INPUT FILE ENTRIES
-            if input_file:
-                if InputPath == 'false':
-                    if input_file[-9:] != '.fastq.gz' and input_file[-6:] != '.fq.gz' and input_file[-4:] != ".bam":
-                        print_error("FastQ file does not have extension '.fastq.gz' or '.fq.gz' or '.bam'!",line)
-                        sys.exit(1)
-                    sample = input_file.split('/')[-1].split('.')[0]
-                else:
-                    sample = input_file
-                    input_file = ''
-                    if replicate:
-                        sample += "REP"+replicate
+                if group.find(" ") != -1:
+                    print_error("Group entry contains spaces!", 'Line', line)
             else:
-                if group:
-                    sample = group
-                    if replicate:
-                        sample += "REP"+replicate
-                else:
-                    print_error("Input file has not been specified!",line)
-                    sys.exit(1)
+                print_error("Group entry has not been specified!", 'Line', line)
 
-            ## CHECK BARCODE ENTRIES
+            ## Check replicate entry is integer
+            if not replicate.isdigit():
+                print_error("Replicate id not an integer!", 'Line', line)
+            replicate = int(replicate)
+
+            ## Check barcode entry
             if barcode:
                 if not barcode.isdigit():
-                    print_error("Barcode entry is not an integer!",line)
-                    sys.exit(1)
+                    print_error("Barcode entry is not an integer!", 'Line', line)
                 else:
                     barcode = 'barcode%s' % (barcode.zfill(2))
 
-            ## CHECK GENOME ENTRIES
+            ## Check input file extension
+            if input_file:
+                if input_file.find(" ") != -1:
+                    print_error("Input file contains spaces!", 'Line', line)
+                if not input_file.endswith(".fastq.gz") and not input_file.endswith(".fq.gz") and not input_file.endswith(".bam"):
+                    print_error("Input file does not have extension '.fastq.gz', '.fq.gz' or '.bam'!", 'Line', line)
+
+            ## Check genome entries
             if genome:
                 if genome.find(' ') != -1:
-                    print_error("Genome entry contains spaces!",line)
-                    sys.exit(1)
-
+                    print_error("Genome entry contains spaces!",'Line', line)
                 if len(genome.split('.')) > 1:
                     if genome[-6:] != '.fasta' and genome[-3:] != '.fa' and genome[-9:] != '.fasta.gz' and genome[-6:] != '.fa.gz':
-                        print_error("Genome entry does not have extension '.fasta', '.fa', '.fasta.gz' or '.fa.gz'!",line)
-                        sys.exit(1)
-
-            ## CHECK TRANSCRIPTOME ENTRIES
+                        print_error("Genome entry does not have extension '.fasta', '.fa', '.fasta.gz' or '.fa.gz'!",'Line', line)
+                        
+            ## Check transcriptome entries
             gtf = ''
             is_transcripts = '0'
             if transcriptome:
-
                 if transcriptome.find(' ') != -1:
-                    print_error("Transcriptome entry contains spaces!",line)
-                    sys.exit(1)
-
+                    print_error("Transcriptome entry contains spaces!",'Line',line)
                 if transcriptome[-6:] != '.fasta' and transcriptome[-3:] != '.fa' and transcriptome[-9:] != '.fasta.gz' and transcriptome[-6:] != '.fa.gz' and transcriptome[-4:] != '.gtf' and transcriptome[-7:] != '.gtf.gz':
-                    print_error("Transcriptome entry does not have extension '.fasta', '.fa', '.fasta.gz', '.fa.gz', '.gtf' or '.gtf.gz'!",line)
-                    sys.exit(1)
-
+                    print_error("Transcriptome entry does not have extension '.fasta', '.fa', '.fasta.gz', '.fa.gz', '.gtf' or '.gtf.gz'!", 'Line', line)
                 if transcriptome[-4:] == '.gtf' or transcriptome[-7:] == '.gtf.gz':
                     gtf = transcriptome
                     if not genome:
-                        print_error("If genome isn't provided, transcriptome must be in fasta format for mapping!",line)
-                        sys.exit(1)
+                        print_error("If genome isn't provided, transcriptome must be in fasta format for mapping!", 'Line', line)
                 else:
                     is_transcripts = '1'
                     genome = transcriptome
 
-            outLines.append([sample,group,replicate,input_file,barcode,genome,gtf,is_transcripts])
-        else:
-            fin.close()
-            break
+            ## Create sample mapping dictionary = {group: {replicate : [ barcode, input_file, genome, gtf, is_transcripts ]}}
+            sample_info = [barcode, input_file, genome, gtf, is_transcripts]
+            if group not in sample_info_dict:
+                sample_info_dict[group] = {}
+            if replicate not in sample_info_dict[group]:
+                sample_info_dict[group][replicate] = sample_info
+            else:
+                print_error("Same replicate id provided multiple times!", 'Line', line)
+                
+    ## Write validated samplesheet with appropriate columns
+    if len(sample_info_dict) > 0:
+        out_dir = os.path.dirname(file_out)
+        make_dir(out_dir)
+        with open(file_out, "w") as fout:
 
-    ## WRITE TO FILE
-    fout = open(FileOut,'w')
-    fout.write(','.join(['sample','group','replicate','input_file','barcode','genome','gtf','is_transcripts']) + '\n')
-    for line in outLines:
-        fout.write(','.join(line) + '\n')
-    fout.close()
-    outfile=open("total_conditions.csv","w")
-    if len(groups) > 0:
-         num_samp = next(iter(groups.values()))
-         if num_samp >= 3 and all(samples == num_samp for samples in groups.values()):
-             outfile.write(",".join(groups)+"\n")
-         else:
-             outfile.write("false")
-    else:
-         outfile.write("false")
+            fout.write(",".join(['sample', 'barcode', 'input_file', 'genome', 'gtf', 'is_transcripts'] + "\n")
+            for sample in sorted(sample_info_dict.keys()):
+
+                ## Check that replicate ids are in format 1..<NUM_REPS>
+                uniq_rep_ids = set(sample_info_dict[sample].keys())
+                if len(uniq_rep_ids) != max(uniq_rep_ids):
+                    print_error("Replicate ids must start with 1..<num_replicates>!", 'Group', sample)
+
+                ### Write to file
+                for replicate in sorted(sample_info_dict[sample].keys()):
+                    sample_id = "{}_R{}".format(sample,replicate)
+                    fout.write(','.join([sample_id] + sample_info_dict[sample][replicate]) + '\n')
+
+#     ## WRITE TO FILE
+#     fout = open(FileOut,'w')
+#     fout.write(','.join(['sample','group','replicate','input_file','barcode','genome','gtf','is_transcripts']) + '\n')
+#     for line in outLines:
+#         fout.write(','.join(line) + '\n')
+#     fout.close()
 
 def main(args=None):
     args = parse_args(args)
-    check_samplesheet(args.FILE_IN,args.FILE_OUT,args.INPUT_PATH)
+    check_samplesheet(args.FILE_IN, args.FILE_OUT)
+
 
 if __name__ == '__main__':
     sys.exit(main())
