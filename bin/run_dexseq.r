6d16e5524495e3858c5cbdc8a46a7faf93111014
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-10-29 16:01:35 +0800

    change it to Harshil's version

--- bin/run_dexseq.r
+++ bin/run_dexseq.r
@@ -1,44 +1,18 @@
 #!/usr/bin/env Rscript
 
-################################################
-################################################
-## REQUIREMENTS                               ##
-################################################
-################################################
-
-## DIFFERENTIAL TRANSCRIPT EXPRESSION ANALYSIS
-    ## - TRANSCRIPT COUNT QUANTIFICATION INPUTS FROM EITHER STRINGTIE2+FEATURECOUNTS OR BAMBU
-    ## - SAMPLE INFORMATION INCLUDING CONDITIONS
-
-################################################
-################################################
-## LOAD LIBRARIES                             ##
-################################################
-################################################
 library(DRIMSeq)
 library(DEXSeq)
 library(stageR)
 
-################################################
-################################################
-## PARSE COMMAND-LINE PARAMETERS              ##
-################################################
-################################################
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) < 3) {
   stop("Please input the directory with the transcript level featureCounts results and the sample information file", call.=FALSE)
 } 
+
 ######### import data ###########
 ##Import featureCounts data
-#path <- "~/Downloads/nanorna-bam-master/mod/featureCounts_transcript/"
 transcriptquant <- args[1]
 path<-args[2]
-
-###################################################
-###################################################
-## FORMAT TRANSCRIPT COUNT QUANTIFICATION OUTPUT ##
-###################################################
-###################################################
 if (transcriptquant == "stringtie2"){
   count.matrix <- data.frame(read.table(path,sep="\t",header=TRUE, skip = 1))
   count.matrix$Chr <- count.matrix$Start <- count.matrix$End <- count.matrix$Length <- count.matrix$Strand <- NULL
@@ -51,29 +25,17 @@ if (transcriptquant == "bambu"){
 }
 colnames(count.matrix)[1] <- "feature_id"
 colnames(count.matrix)[2] <- "gene_id"
-
-################################################
-################################################
-## READ IN SAMPLE INFORMATION (CONDITIONS)    ##
-################################################
-################################################
 #sample information
-#sampleinfo<-read.table("~/Downloads/nanorna-bam-master/samples_conditions.csv",sep=",",header=T)
 sampleinfo<-read.table(args[3],sep=",",header=T)
 colnames(sampleinfo)[1] <- "sample_id"
 colnames(sampleinfo)[2] <- "condition"
 condition_names <- sampleinfo[!duplicated(sampleinfo$condition),]$condition
-#condition_names <- c(levels(sampleinfo$condition))
 lgcolName <- "log2fold"
 for (i in length(condition_names):1){
   lgcolName <- paste(lgcolName,condition_names[i],sep='_')
 }
+######### Filtering #############
 
-################################################
-################################################
-## FILTERING                                  ##
-################################################
-################################################
 cat(paste(sampleinfo$sample_id, collapse = " "))
 cat(paste(colnames(count.matrix), collapse = " "))
 d <- dmDSdata(counts=count.matrix, samples=sampleinfo)
@@ -92,11 +54,7 @@ dFilter <- dmFilter(d,
                     min_gene_expr = min_count_gene,
                     min_feature_prop=0.1)
 
-################################################
-################################################
-## RUN DEXSEQ                                 ##
-################################################
-################################################
+########## DEXSeq #########
 formulaFullModel <- as.formula("~sample + exon + condition:exon")
 #formulaReducedModel <- as.formula("~sample + exon + covariate:exon")
 dxd <- DEXSeqDataSet(countData=round(as.matrix(counts(dFilter)[,-c(1:2)])),
@@ -138,7 +96,6 @@ dxr.g <- data.frame(gene=names(qval),qval)
 
 columns <- c("featureID","groupID","pvalue",lgcolName)
 dxr_pval <- as.data.frame(dxr[,columns])
-#head(dxr_pval)
 pConfirmation <- matrix(dxr_pval$pvalue,ncol=1)
 dimnames(pConfirmation) <- list(strp(dxr_pval$featureID),"transcript")
 pScreen <- qval
